e17dbf37b24052bdd217c95a00f35be99cad50de
Slidejiveman <rdnotlaw91@outlook.com>
2017-11-13 17:19:33 -0600

    Parallelized the project and added user input.

--- main.c
+++ main.c
@@ -23,6 +23,10 @@ long long int calculate_number_of_hits(long long int number_tosses) {
     long long int number_of_hits = 0;
     double x, y, distance_squared;
 
+    // Parallelize this loop
+    // number_of_hits must be a reduction variable
+    // the threads will join implicitly after the completion of this block
+# pragma omp parallel for num_threads(8) reduction(+:number_of_hits)
     for (int toss = 0; toss < number_tosses; toss++) {
         x = generate_random();
         y = generate_random();
@@ -47,11 +51,21 @@ double estimate_pi(long long int number_of_hits, long long int number_of_tosses)
 
 int main() {
     srand(time(NULL));
-    long long int number_of_tosses = 100000;
-    long long int number_of_hits =  calculate_number_of_hits(number_of_tosses);
-    double pi_estimate = estimate_pi(number_of_hits, number_of_tosses);
+    double pi_estimate;
+    long long int number_of_tosses = 0;
+    long long int number_of_hits;
 
-    printf("The estimate for pi is: %f", pi_estimate);
+    // read in the number of tosses to attempt as a long long int
+    while (number_of_tosses != -1) {
+        printf("Please input the number of attempts to make at the dart board. Enter -1 to stop. : \n");
+        scanf("%lld", &number_of_tosses);
+        if (number_of_tosses != -1) {
+            // calculate the estimate of pi
+            number_of_hits = calculate_number_of_hits(number_of_tosses);
+            pi_estimate = estimate_pi(number_of_hits, number_of_tosses);
 
+            printf("The estimate for pi is: %f\n", pi_estimate);
+        }
+    }
     return 0;
 }
